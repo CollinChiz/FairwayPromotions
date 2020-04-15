0ec73edb53ac81c0643604a6b6cb22d0abee4b94
chizzy <cwchiz16@gmail.com>
2020-04-15 11:25:25 -0600

    message

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,12 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-patronus`,
+      options: {
+        apiKey: process.env.STRIPE_PUBLISHABLE_KEY,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
