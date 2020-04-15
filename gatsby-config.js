097d197a9e8c8d657ea3b850cc6d01da4660af9e
chizzy <cwchiz16@gmail.com>
2020-04-15 07:25:24 -0600

    message

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@
 //   path: `.env.${activeEnv}`,
 // })
 require("dotenv").config({
-    path: `/.env.${process.env.NODE_ENV}`,
+    path: `\.env.${process.env.NODE_ENV}`,
   })
 
 module.exports = {
