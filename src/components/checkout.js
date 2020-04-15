ccc746a54ffadfd2c21a8e94b4b887769098795c
chizzy <cwchiz16@gmail.com>
2020-04-15 09:31:36 -0600

    messae

--- src/components/checkout.js
+++ src/components/checkout.js
@@ -15,7 +15,7 @@ const buttonStyles = {
 
 const apiKey = process.env.STRIPE_PUBLISHABLE_KEY
 const stripePromise = loadStripe(apiKey)
-console.log(apiKey);
+console.log(process.env);
 const redirectToCheckout = async event => {
   
   event.preventDefault()
