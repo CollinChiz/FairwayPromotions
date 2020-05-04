7bf00f0fc25731b44c18b49e44e760f72f322639
chizzy <cwchiz16@gmail.com>
2020-05-04 15:08:09 -0600

    chane

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -1,2 +1,2 @@
 const fs = require('fs')
-fs.writeFileSync('./.env', `API_KEY=${process.env.STRIPE_PUBLISHABLE_KEY}\n`)
+fs.writeFileSync('./.env', `STRIPE_PUBLISHABLE_KEY=${process.env.STRIPE_PUBLISHABLE_KEY}\n`)
