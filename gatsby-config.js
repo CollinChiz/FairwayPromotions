f9671c6318496449958c38b2d087523baf7b0c96
chizzy <cwchiz16@gmail.com>
2020-05-04 14:34:39 -0600

    fixed syntax

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,13 +20,6 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: '.env.production',
-        path: `${__dirname}/.env.production`
-      },
-    }
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
