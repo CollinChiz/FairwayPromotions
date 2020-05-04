bca405c7adeefb4eb71698509b65220bb2f9be0f
chizzy <cwchiz16@gmail.com>
2020-05-04 14:33:37 -0600

    dev query

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,13 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `.env.production`,
+        path: `${__dirname}/.env.production`,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
