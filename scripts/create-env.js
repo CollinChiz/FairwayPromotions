d13e4882b1eaca59bb56179a45f5133c22678df4
chizzy <cwchiz16@gmail.com>
2020-05-04 15:22:57 -0600

    change

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -1,2 +1,2 @@
 const fs = require('fs')
-fs.writeFileSync('./.env', `STRIPE_PUBLISHABLE_KEY=${process.env.STRIPE_PUBLISHABLE_KEY}\n`)
+fs.writeFileSync('./.env', `GATSBY_STRIPE_PUBLISHABLE_KEY=${process.env.GATSBY_STRIPE_PUBLISHABLE_KEY}\n`)
--- src/components/checkout.js
+++ src/components/checkout.js
@@ -13,7 +13,7 @@ const buttonStyles = {
   letterSpacing: "1.5px",
 }
 
-const stripePromise = loadStripe(process.env.STRIPE_PUBLISHABLE_KEY)
+const stripePromise = loadStripe(process.env.GATSBY_STRIPE_PUBLISHABLE_KEY)
 const redirectToCheckout = async event => {
   
   event.preventDefault()
